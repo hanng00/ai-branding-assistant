c32decc4756166568e986a180eca091d541959c7
Hannes Gustafsson <hannes.gustafsson@burtcorp.com>
2022-10-30 12:55:59 +0100

    Add FastAPI

---
+++ app/api.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, HTTPException
+from open_ai_integration import (
+    MAX_INPUT_LENGTH,
+    generate_branding_snippet,
+    generate_keywords,
+)
+
+app = FastAPI()
+MAX_INPUT_LENGTH = 32
+
+
+@app.get("/generate_snippet")
+async def generate_snippet_api(prompt: str):
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    return {"snippet": snippet, "keywords": []}
+
+
+@app.get("/generate_keyword")
+async def generate_keywords_api(prompt: str):
+    validate_input_length(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": None, "keywords": keywords}
+
+
+@app.get("/generate_snippet_and_keyword")
+async def generate_keywords_api(prompt: str):
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": snippet, "keywords": keywords}
+
+
+def validate_input_length(prompt: str):
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Input length too long. Must be under {MAX_INPUT_LENGTH} characters",
+        )
+
+
+# uvicorn api:app --reload
