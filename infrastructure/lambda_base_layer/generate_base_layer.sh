497c536c26d9f0e355c88be6dc4ff413121decbb
Hannes Gustafsson <hannes.gustafsson@burtcorp.com>
2022-10-31 12:17:53 +0100

    Add Lambda Base Layer

---
+++ infrastructure/lambda_base_layer/generate_base_layer.sh
@@ -0,0 +1,12 @@
+
+# Remove the container first if it exists
+docker rm layer-container
+
+# Build the base layer
+docker build -t base-layer .
+
+# Rename it to layer-container
+docker run --name layer-container base-layer
+
+# COpy the generated zip artifact so our CDK can use it
+docker cp layer-container:layer.zip . && echo "Created layer.zip with updated base layer."
